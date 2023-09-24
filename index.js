6e31b03a3142ba1a60a24112db44aa77b0501e77
NELSON RIVERA <nrrvrivera@hotmail.com>
2023-09-23 18:44:52 -0600

    Agregado package.json

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+function requestController() {
+console.log('Recibimos una nueva request');
+};
+
+const server = http.createServer(requestController);
+server.listen(4000);
